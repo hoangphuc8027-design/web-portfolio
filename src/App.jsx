b93b19ee7a860f92903ef987a49d80fd0b481a5f
tyfon1999 <hoangphuc8027@gmail.com>
2026-05-15 10:55:40 +0700

    Update ready

--- src/App.jsx
+++ src/App.jsx
@@ -102,7 +102,7 @@ const clientFiles = [
     fileName: "No source",
     type: "Chưa có sẵn",
     size: "~ ~",
-    status: "Ready",
+    status: "Processing",
     updated: "14/05/2026",
     link: "https://drive.google.com",
     password: "lumine2026",
